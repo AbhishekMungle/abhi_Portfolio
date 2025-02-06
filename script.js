3f88d9e7262c67a17db7f8a0615f0c72252b08df
Abhishek Mungale <abhishekmungale624@gmail.com>
2025-02-07 00:21:08 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+
+
+    let gal = document.querySelector(".gal");
+    let cal = document.querySelector(".cal");
+    let flip = document.querySelector(".flip");
+    let tic = document.querySelector(".tic");
+    let port = document.querySelector(".port");
+    let stop = document.querySelector(".stop");
+    let todo = document.querySelector(".todo");
+    flip.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/flipcart_clone/";
+    });
+
+    cal.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/CodeAlpha_Task2/";
+    });
+    tic.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/SCT_WD3/";
+    });
+    gal.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/codeAlph_1/";
+    });
+    
+    port.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/CodeSoft1/";
+    });
+    stop.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/SCT_WD2/";
+    });
+    todo.addEventListener("click", () => {
+        window.location.href = "https://abhishekmungle.github.io/SCWD-4-todolist/";
+    });
